12af53d448891254ac4d5111954f15d6de1a99d6
Sabarivasan <slsabarivasan30@gmail.com>
2025-03-26 09:38:37 +0530

    Update asgi.py and settings for Channels support

--- chatbot/consumers.py
+++ chatbot/consumers.py
@@ -1,5 +1,5 @@
 # chatbot/consumers.py
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer # type: ignore
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
@@ -11,3 +11,12 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         # Echo back the message (replace with chatbot logic later)
         self.send(text_data=f"Echo: {text_data}")
+
+
+
+        # 1a44b32a413ec5fc7cbe06bea6730695 key in render.com
+        # Static Outbound IP Addresses
+        # 35.160.120.126
+        # 44.233.151.27
+        # 34.211.200.85
+        # https://chat-with-web-gl1v.onrender.com link for project
